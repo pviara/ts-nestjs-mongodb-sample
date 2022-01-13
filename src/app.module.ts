ccf5f6d7438a996fffa21e2f32c15c7a05f9aa5b
Pierre Viara <pierre.viara@outlook.com>
2022-01-13 18:47:09 +0100

    feature(config): ⚙️ connect the app with the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,27 @@
 import { AppConfigurationModule } from './infrastructure/configuration/app-configuration.module';
+import { AppConfigurationService } from './infrastructure/configuration/app-configuration.service';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Module } from '@nestjs/common';
+import { MongooseModule, MongooseModuleOptions } from '@nestjs/mongoose';
 
 @Module({
-  imports: [AppConfigurationModule],
+  imports: [
+    AppConfigurationModule,
+    MongooseModule.forRootAsync({
+      imports: [AppConfigurationModule],
+      inject: [AppConfigurationService],
+      useFactory: (appConfigService: AppConfigurationService) => {
+        const options: MongooseModuleOptions = {
+          uri: appConfigService.connectionString,
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        };
+        return options;
+      }
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
