ec9f63abd85d881b77ab5d6c499e4699ac6850cf
Pierre Viara <pierre.viara@outlook.com>
2022-01-14 13:05:36 +0100

    feature(user): 🧑 add user repository

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserRepository } from './user.repository';
 import { userSchema } from './user';
 
 @Module({
@@ -10,6 +11,6 @@ import { userSchema } from './user';
       name: 'User', schema: userSchema
     }])
   ],
-  providers: []
+  providers: [UserRepository]
 })
 export class UserModule { }
